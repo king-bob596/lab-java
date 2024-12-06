ff99cd3fe883f4ae40573d1632554b24d84c615e
king-bob596 <claytonn381@my.clevelandcc.edu>
2024-12-06 09:10:06 -0500

    Add files via upload

---
+++ java lab/script.js
@@ -0,0 +1,10 @@
+function smallchangefont(){
+    document.getElementById("name").style.fontSize="xx-small";}
+    
+    function mediumchangefont(){
+        document.getElementById("name").style.fontSize="medium";
+    
+    }
+    function largechangefont(){
+        document.getElementById("name").style.fontSize="x-large";
+    }
